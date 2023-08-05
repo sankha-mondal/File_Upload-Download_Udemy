2f9819a963b3d7da1788066ccb5fadc4cc7a5d47
Sankha Subhra Mondal <sankha123@gmail.com>
2023-08-05 11:37:37 +0530

    File Upload-Download Udemy

---
+++ File_Upload-Download_Udemy/src/main/java/com/FileUploadDownloadUdemyApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadUdemyApplication.class, args);
+		System.out.println("File Upload Download Udemy Application Running on port No 8080...");
+	}
+}
